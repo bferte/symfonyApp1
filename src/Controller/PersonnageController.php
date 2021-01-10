5ed196766805c582fe692c5670b9e27b8437c3a9
Briac ferté <bferte@macbook-pro-de-briac.home>
2021-01-10 21:06:09 +0100

    10/01/21

---
+++ src/Controller/PersonnageController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PersonnageController extends AbstractController
+{
+    /**
+     * @Route("/", name="personnage")
+     */
+    public function index(): Response
+    {
+        return $this->render('personnage/index.html.twig', [
+            'controller_name' => 'PersonnageController',
+        ]);
+    }
+
+    /**
+     * @Route("/persos", name="persos")
+     */
+    public function persos(): Response
+    {
+        return $this->render('personnage/persos.html.twig', [
+            'controller_name' => 'PersonnageController',
+        ]);
+    }
+}
